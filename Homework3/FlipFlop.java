3ee1e024e57845ecac7dd3176f20b35f81818858
denisse-br <denisse294@live.com>
2014-02-08 20:04:13 -0600

    tarea3_201224240

--- Homework3/FlipFlop.java
+++ Homework3/FlipFlop.java
@@ -1,33 +1,14 @@
-/*
-Introduccion a la computacion y programacion 1 seccion B
-Laboratorio, algoritmos en java
-
-Complete el algoritmo:
-
-Se ingresara un numero, el programa debe iterar desde cero hasta ese numero y mostrar el mensaje
-Flip si el numero es divisible dentro de 3
-Flop si el numero es divisible dentro de 5
-FlipFlop si el numero es divisible dentro de 3 y 5
-y si no cumple ninguna de estas condiciones, solo mostrar el valor de ese numero.
-*/
-
-public class FlipFlop {   
-
-   public static void main(String[] args) {
-   		int numeroActual = 0;
-   		System.out.println(args[0]);
-
-   		// si el numero es divisible dentro de 3 imprima "Flip"
-			System.out.println("Flip");
-
-   		// si el numero es divisible dentro de 5 imprima "Flop"
-			System.out.println("Flop");
-
-   		// si el numero es divisible dentro de 3 y 5 imprima "FlipFlop"
-			System.out.println("FlipFlop");
-
-   		// de lo contrario, imprima el numero
-      		System.out.println(numeroActual);
+public class FlipFlop {
+   public static void main(String[] args){
+      for(int x=1;x <=100; x++){
+         if(((x%3) == 0) && ((x%5)== 0))
+            System.out.println("FlipFlop");
+         else if ((x%3)==0)
+         System.out.println("Flip");
+         else if ((x%5)==0)
+         System.out.println("Flop");
+         else
+            System.out.println(x);
+      }
    }
-
 }
